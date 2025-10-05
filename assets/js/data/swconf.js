253998366596b32f029f133bf1572739f395517c
chunfengfusu <2023012685@nwafu.edu.cn>
2025-10-05 06:06:34 +0000

    更新 Jekyll 站点编译结果

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1759641543',resources: [
+    cacheName: 'chirpy-1759644265',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
